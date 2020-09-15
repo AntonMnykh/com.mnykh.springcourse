31e36a968f050dcb84fd878560f7adf21fa3ce12
AntonMnykh <AntonMnykh@epam.com>
2020-09-15 16:22:53 +0300

    Results of lesson #8

--- src/main/java/com/mnykh/springcourse/ClassicalMusic.java
+++ src/main/java/com/mnykh/springcourse/ClassicalMusic.java
@@ -2,6 +2,20 @@ package com.mnykh.springcourse;
 
 public class ClassicalMusic implements Music{
 
+    private ClassicalMusic() {}
+
+    public static ClassicalMusic getClassicalMusic() {
+        return new ClassicalMusic();
+    }
+
+    public void init() {
+        System.out.println("Bean Initialization");
+    }
+
+    public void destroy() {
+        System.out.println("Bean Destruction");
+    }
+
     @Override
     public String getSong() {
         return "Hungarian Rhapsody";
